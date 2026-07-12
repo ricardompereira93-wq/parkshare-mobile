6143ee2459d5cec086106d60676d09fcdddc2097
Ricardo Pereira <ricardompereira93@gmail.com>
2026-07-12 15:05:37 +0200

    Supabase configurado

---
+++ lib/supabase_config.dart
@@ -0,0 +1,7 @@
+class SupabaseConfig {
+  static const String supabaseUrl =
+      'https://cxaerpemrfvbeqqozgoj.supabase.co';
+
+  static const String supabaseAnonKey =
+      'sb_publishable_v4kbFK1EpIKjeY1cPjbFvg_ShACR3Ss';
+}
